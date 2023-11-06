0c87d7115ae45ab43ae2f3bb93484256142255f1
Austin <austinybyrdy@gmail.com>
2023-11-06 08:03:33 -0700

    Assignment

--- ecommerce_app/ecommerce_app/urls.py
+++ ecommerce_app/ecommerce_app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     # Default
     path('', views.index, name='index'),
 
-    # List
-    path('products/', views.ProductListView.as_view(), name='products'),
+    # # List
+    # path('products/', views.ProductListView.as_view(), name='products'),
 
     # Details url, view, and name to call from view
     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
